1397a9d8fa360fecb6dd7557428f09057f46ef3e
xhunn <xhunn@10.12.14.224>
2022-09-09 16:48:20 +0530

    New Sort

--- src/NewSort.java
+++ src/NewSort.java
@@ -1,9 +1,17 @@
-
-public class NewSort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+// Importing class Arrays of the sub-package util.
+import java.util.Arrays;
+public class SortingExample
+{             
+public static void main(String argvs[])
+{
+               // String array
+               String[] str = { "hjk", "ty", "trf", "tyf", "ytf", "bnm", "uio", "oml", "tkt" };
+               // Integer Array
+               int[] intAr = { 31, 5, 86, 405, 211, 19, 171, 192, 67 };
+               Arrays.sort(str); // Sorting string array
+               Arrays.sort(intAr);  // Sorting integer array
+               // Displaying results
+               System.out.printf("Sorted str[] : %s \n", Arrays.toString(str)); 
+                System.out.printf("Modified intAr[] : %s", Arrays.toString(intAr));
+}
 }
